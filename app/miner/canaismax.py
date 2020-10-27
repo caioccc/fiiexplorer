2c96300454b140bb760c8a8dfebd3e8bb6ad3c2c
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2020-10-27 14:20:11 -0300

    add test

--- app/miner/canaismax.py
+++ app/miner/canaismax.py
@@ -27,28 +27,44 @@ class CustomMiner(Miner):
                             img_url = img_tag['data-src']
                         else:
                             img_url = ''
-                        second_page = self.get_page_bs4(href)
-                        if second_page:
-                            div_links = second_page.find_all('a', attrs={'class': 'cativ'})
-                            if div_links:
-                                atags = div_links
-                                if len(atags) > 0:
-                                    ids = []
-                                    for a in atags:
-                                        if a.has_attr('data-link'):
-                                            data_id = str(a['data-link'])
-                                            ids.append(data_id)
-                                    if len(ids) > 0:
-                                        ch = Channel()
-                                        ch.title = title
-                                        ch.img_url = img_url
-                                        ch.category = category
-                                        ch.save()
-                                        for id_url in ids:
-                                            link = Link()
-                                            link.url = id_url
-                                            link.channel = ch
-                                            link.save()
+                        if 'assistir-israel-adesanya' not in href:
+                            second_page = self.get_page_bs4(href)
+                            if second_page:
+                                div_links = second_page.find_all('a', attrs={'class': 'cativ'})
+                                if div_links:
+                                    atags = div_links
+                                    if len(atags) > 0:
+                                        ids = []
+                                        for a in atags:
+                                            if a.has_attr('data-link'):
+                                                data_id = str(a['data-link'])
+                                                if 'op=' in data_id:
+                                                    third_page_temp = self.get_page_bs4(data_id)
+                                                    if third_page_temp:
+                                                        div_buttons = third_page_temp.find('div',
+                                                                                           attrs={
+                                                                                               'class': 'buttons-quality'})
+                                                        if div_buttons:
+                                                            novos_links = div_buttons.find_all('a')
+                                                            if len(novos_links) > 0:
+                                                                for alinks_temp in novos_links:
+                                                                    if alinks_temp.has_attr('href'):
+                                                                        alink_temp_url = str(alinks_temp['href'])
+                                                                        ids.append(alink_temp_url)
+                                                else:
+                                                    print('normal', title)
+                                                    ids.append(data_id)
+                                        if len(ids) > 0:
+                                            ch = Channel()
+                                            ch.title = title
+                                            ch.img_url = img_url
+                                            ch.category = category
+                                            ch.save()
+                                            for id_url in ids:
+                                                link = Link()
+                                                link.url = id_url
+                                                link.channel = ch
+                                                link.save()
 
     def mine(self):
         try:
