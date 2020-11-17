b758ae003bf5229706b3df65d32e3bc8289130ad
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2020-11-17 12:53:50 -0300

    add debug

--- app/management/commands/delete.py
+++ app/management/commands/delete.py
@@ -8,18 +8,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            # Site.objects.all().delete()
-            # CategoryChannel.objects.all().delete()
+            Site.objects.all().delete()
+            CategoryChannel.objects.all().delete()
             Channel.objects.all().delete()
             Link.objects.all().delete()
-            # Filme.objects.all().delete()
-            # Url.objects.all().delete()
-            # User.objects.all().delete()
-            # Filme.objects.all().delete()
-            # Serie.objects.all().delete()
-            # Temporada.objects.all().delete()
-            # Episodio.objects.all().delete()
-            # LinkSerie.objects.all().delete()
+            Filme.objects.all().delete()
+            Url.objects.all().delete()
+            User.objects.all().delete()
+            Filme.objects.all().delete()
+            Serie.objects.all().delete()
+            Temporada.objects.all().delete()
+            Episodio.objects.all().delete()
+            LinkSerie.objects.all().delete()
         except (Exception,):
             raise CommandError('Erro ao deletar Models')
         self.stdout.write(self.style.SUCCESS('Successfully deleted'))
